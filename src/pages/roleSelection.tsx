12d6a09d6ea36647487f6411c1e6381c2dd66697
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-01-29 21:34:29 -0600

    feat(roleselection): added Role Selection Page

--- src/pages/roleSelection.tsx
+++ src/pages/roleSelection.tsx
@@ -1,35 +1,55 @@
-import {
-    Flex, Heading, Text, Button
-} from '@chakra-ui/react'
+import { Flex, Heading, Text, Button } from '@chakra-ui/react';
 
-export default function roleSelection(){
-    return (
+export default function roleSelection() {
+  return (
+    <Flex
+      flexDir="column"
+      alignItems="center"
+      justifyContent="space-around"
+      h={['80%', '50%']}
+    >
+      <Heading fontWeight="400" color="white" fontSize={['35px', '48px']}>
+        Select Your Roles
+      </Heading>
       <Flex
-        flexDir="column"
+        flexDirection="column"
+        justifyContent="space-between"
         alignItems="center"
-        justifyContent="space-around"
-        w="571px"
-        h="564px"
+        background="linear-gradient(179.99deg, #1D2D50 0.01%, #52057B 193.87%)"
+        padding="40px 32px"
+        borderRadius="30px"
+        w="311px"
+        h="352px"
       >
-        <Heading fontWeight="400" color="white">
-          Select Your Roles
-        </Heading>
-        <Flex
-          flexDirection="column"
-          alignItems="center"
-          background="linear-gradient(179.99deg, #1D2D50 0.01%, #52057B 193.87%)"
-          borderRadius="30px"
-          padding="21px 32px"
-        >
-          <Text color="white">Hard Support</Text>
-          <Text color="white">Soft Support</Text>
-          <Text color="white">Offlane</Text>
-          <Text color="white">Midlane</Text>
-          <Text color="white">Hard Carry</Text>
-        </Flex>
-        <Button backgroundColor="pink" size="lg">
-          Search
-        </Button>
+        <Text lineHeight="33px" fontSize="24px" color="white">
+          Hard Support
+        </Text>
+        <Text lineHeight="33px" fontSize="24px" color="white">
+          Soft Support
+        </Text>
+        <Text lineHeight="33px" fontSize="24px" color="white">
+          Offlane
+        </Text>
+        <Text lineHeight="33px" fontSize="24px" color="white">
+          Midlane
+        </Text>
+        <Text lineHeight="33px" fontSize="24px" color="white">
+          Hard Carry
+        </Text>
       </Flex>
-    );
+      <Button
+        backgroundColor="pink"
+        size="lg"
+        h="70px"
+        w="260px"
+        borderRadius="30px"
+        fontSize="36px"
+        fontFamily="Montserrat"
+        fontWeight="normal"
+        color="white"
+      >
+        Search
+      </Button>
+    </Flex>
+  );
 }
