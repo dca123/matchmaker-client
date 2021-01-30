78b3707bfa539694bdff7735f7ed67f1d3919936
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-01-29 19:27:19 -0600

    feat(_app.tsx): added theme & basic homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 
 import theme from '../theme'
+import Fonts from '../theme/fonts'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
+      <Fonts />
+      <Flex
+        height="100vh"
+        backgroundColor="black"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Component {...pageProps} />
+      </Flex>
     </ChakraProvider>
   )
 }
