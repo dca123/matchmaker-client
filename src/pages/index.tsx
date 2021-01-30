ed90c2024aa1430d616f3a757f1a65a56eac2c68
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-01-29 21:33:48 -0600

    feat(index.tsx): updated theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,30 @@
 import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
   Image,
   Flex,
   Heading,
   Button,
-  Box,
-  Link,
-} from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
-import { Hero } from "../components/Hero";
-import { Container } from "../components/Container";
-import { Main } from "../components/Main";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
-import { CTA } from "../components/CTA";
-import { Footer } from "../components/Footer";
-
-const Index = () => (
-  <Flex
-    flexDirection="column"
-    justifyContent="space-around"
-    alignItems="center"
-    background="linear-gradient(179.99deg, #1D2D50 0.01%, #52057B 193.87%);"
-    w="571px"
-    h="564px"
-    borderRadius="30px"
-  >
-    <Heading size="3xl" color="white">DOTA Newbs</Heading>
-    <Image h="120px" w="120px" src="dotaImg.png" />
-    <Button backgroundColor="pink" size="lg">Login Via Steam</Button>
-  </Flex>
-);
-
-export default Index;
+export default function Index() {
+  const buttonSize = useBreakpointValue({ base: 'xs', sm: 'sm', md: 'md' });
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="space-around"
+      alignItems="center"
+      background="linear-gradient(179.99deg, #1D2D50 0.01%, #52057B 193.87%);"
+      w="50%"
+      h="50%"
+      borderRadius="30px"
+    >
+      <Heading size={buttonSize} color="white" animate={{ scale: 0.5 }}>
+        DOTA Newbs
+      </Heading>
+      <Image h="120px" w="120px" src="dotaImg.png" />
+      <Button backgroundColor="pink" size={buttonSize}>
+        Login Via Steam
+      </Button>
+    </Flex>
+  );
+}
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,27 +1,27 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
-const fonts = { 
+const fonts = {
   heading: 'Montserrat',
-  body: 'Nunito'
- }
+  body: 'Nunito',
+};
 
 const breakpoints = createBreakpoints({
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-})
+});
 
 const theme = extendTheme({
   colors: {
     black: '#121C30',
     blue: '#1D2D50',
     purple: '#52057B',
-    pink: '#BC6FF1'
+    pink: '#BC6FF1',
   },
   fonts,
   breakpoints,
-})
+});
 
-export default theme
+export default theme;
