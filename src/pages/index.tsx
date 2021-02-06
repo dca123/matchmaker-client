0c62dd910b0ee70105033f18d878fe9b3cddb884
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-02-06 01:05:06 -0600

    feat(components/nextchakralink.tsx): new component combining next & chakra link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { Image, Heading } from '@chakra-ui/react';
 import { Button, GradientCard } from '../components/CustomComponents';
+import { useRouter } from 'next/dist/client/router';
 export default function Index() {
+  const router = useRouter();
   return (
     <GradientCard h="564px" w="571px">
       <Heading fontSize={['36px', '64px']} color="white">
         DOTA Newbs
       </Heading>
       <Image h="120px" w="120px" src="dotaImg.png" />
-      <Button fontSize="24px">Login Via Steam</Button>
+      <Button fontSize="24px" onClick={() => router.push('/searching')}>
+        Login via Steam
+      </Button>
     </GradientCard>
   );
 }
