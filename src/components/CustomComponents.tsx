89ac22fd4a4aeca9eef7fcad7623fcb39950f863
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-02-06 01:02:12 -0600

    feat(customcomponents.tsx): now using chakra ui provided props as opposed to custom defined props

--- src/components/CustomComponents.tsx
+++ src/components/CustomComponents.tsx
@@ -1,2 +1,3 @@
 export { default as Button } from './Button';
 export { default as GradientCard } from './GradientCard';
+export { default as NextChakraLink } from './NextChakraLink';
--- src/components/GradientCard.tsx
+++ src/components/GradientCard.tsx
@@ -1,20 +1,16 @@
-import { Flex } from '@chakra-ui/react';
-type GradientCardProps = {
-  children: React.ReactNode;
-  h: string;
-  w: string;
-};
-export default function GradientCard({ children, h, w }: GradientCardProps) {
+import { Flex, FlexProps } from '@chakra-ui/react';
+export default function GradientCard({ children, h, w, ...rest }: FlexProps) {
   return (
     <Flex
       flexDirection="column"
       justifyContent="space-around"
       alignItems="center"
       background="linear-gradient(179.99deg, #1D2D50 0.01%, #52057B 193.87%)"
-      padding="40px 32px"
+      // padding="10px 16px"
       borderRadius="30px"
       w={w}
       h={h}
+      {...rest}
     >
       {children}
     </Flex>
