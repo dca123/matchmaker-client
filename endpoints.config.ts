8eee61fae0197bb67a27c78f5b5ff1eb34a51754
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-08 18:11:00 -0600

    feat(pages/api/auth/[...nextauth].js): added Next auth with Discord configs

---
+++ endpoints.config.ts
@@ -0,0 +1,5 @@
+export default {
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? '',
+  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID ?? '',
+  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET ?? '',
+};
