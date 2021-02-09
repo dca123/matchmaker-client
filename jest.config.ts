22a1b902d555a9b7ebe760779e00e38b6d88ae19
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-09 17:51:51 -0600

    feat(jest.config.ts): init jest & testing libary with required configs

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/components(.*)$': '<rootDir>/src/components$1',
+    '^@/pages(.*)$': '<rootDir>/src/pages$1',
+  },
+};
+export default config;
---
+++ test/pages/auth_index.test.tsx
@@ -0,0 +1,18 @@
+import Index from '@/pages/index';
+import { render, screen } from '@testing-library/react';
+import client, { Session } from 'next-auth/client';
+
+jest.mock('next-auth/client');
+describe('App', () => {
+  it('renders without crashing', () => {
+    const mockSession: Session = {
+      expires: '1',
+      user: { email: 'a', name: 'Delta', image: 'c' },
+    };
+    (client.useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+    render(<Index />);
+    expect(
+      screen.getByRole('heading', { name: 'Find Me a Lobby' }),
+    ).toBeInTheDocument();
+  });
+});
