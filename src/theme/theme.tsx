6e2ff1cffa1fcba97f136c70888056de822d7304
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-21 07:09:00 -0600

    feat(theme.tsx): added brand color theme
    Still need to remove existing colours and change to brand

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -17,6 +17,18 @@ const colors = {
   blue: '#1D2D50',
   purple: '#52057B',
   pink: '#BC6FF1',
+  brand: {
+    50: '#bc6ff1',
+    100: '#ac61df',
+    200: '#9b54cd',
+    300: '#8a48ba',
+    400: '#783da6',
+    500: '#653592',
+    600: '#502e7b',
+    700: '#3a2963',
+    800: '#222448',
+    900: '#121c30',
+  },
 };
 const theme = extendTheme({
   colors,
