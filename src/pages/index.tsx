1a4644c00b7f4d714f40a97f044b80f455f0e92a
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-03-11 05:53:58 -0600

    feat(index.tsx): integration with api for ticket creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { Dispatch, SetStateAction, useReducer } from 'react';
 import { actionType } from '@/components/SearchConfigButton';
 import { Flex } from '@chakra-ui/react';
 import SearchConfigCard from '@/layouts/SearchConfigCard';
@@ -6,6 +6,8 @@ import { Layout, PageHeading, Button } from '@/components/CustomComponents';
 import withAuth from 'src/hoc/withAuthentication';
 import { Session } from 'next-auth/client';
 import { User } from 'next-auth';
+import { Ticket, useTicket } from 'src/contexts/ticketContext';
+import { NextRouter, useRouter } from 'next/router';
 import endpoint from '../../endpoints.config';
 
 type searchConfigType = {
@@ -55,12 +57,8 @@ const toggleSearchConfig = (
       throw new Error('Unexcepted Action Type');
   }
 };
-const postData = async ({
-  id,
-  steamID,
-}: User): Promise<Record<string, string>> => {
-  console.log(endpoint.NEXT_PUBLIC_API_RMM);
 
+const postData = async ({ id, steamID }: User): Promise<Ticket> => {
   const ticketID = await fetch(`${endpoint.NEXT_PUBLIC_API_RMM}/ticket`, {
     method: 'POST',
     mode: 'cors',
@@ -74,11 +72,24 @@ const postData = async ({
   });
   return ticketID.json();
 };
+const createTicket = async (
+  setTicket: Dispatch<SetStateAction<Ticket>>,
+  session: User,
+  router: NextRouter
+): Promise<void> => {
+  const returnData = await postData(session);
+  setTicket(returnData);
+  sessionStorage.setItem('ticketID', returnData.ticketID);
+  router.push('/searching');
+};
 function Index({ session }: { session: Session }): React.ReactElement {
   const [
     { roleSelection, serverSelection },
     dispatchSearchConfigState,
   ] = useReducer(toggleSearchConfig, defaultSearchConfig);
+  const { setTicket } = useTicket();
+  const router = useRouter();
+
   return (
     <Layout>
       <PageHeading>Are You Ready ?</PageHeading>
@@ -98,7 +109,9 @@ function Index({ session }: { session: Session }): React.ReactElement {
           configTitle="Server"
         />
       </Flex>
-      <Button onClick={() => postData(session.user)}>Search</Button>
+      <Button onClick={() => createTicket(setTicket, session.user, router)}>
+        Search
+      </Button>
     </Layout>
   );
 }
