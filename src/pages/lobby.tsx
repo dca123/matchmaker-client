b28b2a023b206d6da0d0d620bc62342bdfe08865
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-03-12 21:10:14 -0600

    feat(lobby.tsx): shows player ready changes on the card
    Uses the useLayout hook to connect and listen for changes

--- src/pages/lobby.tsx
+++ src/pages/lobby.tsx
@@ -3,65 +3,68 @@ import withAuth from 'src/hoc/withAuthentication';
 import { useRouter } from 'next/router';
 import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
-import { defaultUser, Player } from '@/layouts/TeamCard';
+import { defaultPlayerArray, Player } from '@/layouts/TeamCard';
 import { useTicket } from 'src/contexts/ticketContext';
 import endpointsConfig from 'endpoints.config';
 import { TeamCard, PageHeading, Layout } from '../components/CustomComponents';
 
 function Lobby(): React.ReactElement {
   const router = useRouter();
-  const { ticket } = useTicket();
-  const ticketID = sessionStorage.getItem('ticketID');
 
-  let auth = {
-    ticket,
-  };
-  if (ticketID) {
-    auth = {
-      ticket: {
-        ticketID,
-      },
-    };
-  }
-  const socket = io(`${endpointsConfig.NEXT_PUBLIC_API_RMM}/lobby`, {
-    auth,
-  });
   const [title, setTitle] = useState('Waiting on the Ancients');
-  const [progess, setProgress] = useState(10);
+  const [progess, setProgress] = useState(0);
   const [lobbyError, setLobbyError] = useState(false);
-  const [radiantPlayers, setRadiantPlayers] = useState([
-    defaultUser,
-    defaultUser,
-    defaultUser,
-    defaultUser,
-    defaultUser,
-  ]);
-  socket.on(
-    'lobbyState',
-    (newProgress: number, newTitle: string, newLobbyError?: boolean) => {
-      setTitle(newTitle);
-      setProgress(newProgress);
-      if (newLobbyError) {
-        setLobbyError(newLobbyError);
-      }
-    }
-  );
-  socket.on('radiant update', (newRadiant: Player[]) => {
-    setRadiantPlayers(newRadiant);
-  });
+  const [radiantPlayers, setRadiantPlayers] = useState(defaultPlayerArray);
+  const [direPlayers, setDirePlayers] = useState(defaultPlayerArray);
+  const { ticket } = useTicket();
 
   useEffect(() => {
-    // Disconnect socket on leaving page. Avoids memory leaks on reloads
+    if (!ticket.ticketID) {
+      ticket.ticketID = sessionStorage.getItem('ticketID') ?? '';
+    }
+
+    const socket = io(`${endpointsConfig.NEXT_PUBLIC_API_RMM}/lobby`, {
+      auth: {
+        ticket,
+      },
+    });
+    socket.on('playerList', (players: Player[]) => {
+      setRadiantPlayers(players.slice(0, 5));
+    });
+    socket.on('lobbyState', (newProgress: number, newTitle: string) => {
+      setTitle(newTitle);
+      setProgress(newProgress);
+    });
+    socket.on(
+      'lobbyTimeout',
+      (newProgress: number, newTitle: string, newLobbyError: boolean) => {
+        setTitle(newTitle);
+        setProgress(newProgress);
+        if (newLobbyError) {
+          setLobbyError(newLobbyError);
+        }
+      }
+    );
+    socket.on(
+      'waitingForPlayers',
+      (newProgress: number, newTitle: string, players: Player[]) => {
+        setTitle(newTitle);
+        setProgress(newProgress);
+        setRadiantPlayers(players.slice(0, 5));
+        setDirePlayers(players.slice(5, 10));
+      }
+    );
+
     return () => {
+      // Disconnect socket on leaving page. Avoids memory leaks on reloads
       socket.disconnect();
     };
-  });
+  }, [ticket]);
   return (
     <>
       <Layout>
         <PageHeading>{title}</PageHeading>
         <Progress
-          colorScheme="green"
           value={progess}
           size="sm"
           w={['22rem', '43rem']}
@@ -73,7 +76,7 @@ function Lobby(): React.ReactElement {
         />
         <Flex w={['20rem', '36rem']} justifyContent="space-around">
           <TeamCard teamName="Radiant" playerArray={radiantPlayers} />
-          <TeamCard teamName="Dire" />
+          <TeamCard teamName="Dire" playerArray={direPlayers} />
         </Flex>
         <Heading
           fontSize={['sm', 'lg']}
